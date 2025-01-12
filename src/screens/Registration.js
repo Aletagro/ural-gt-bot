1f96689cb85462f0bcfaa99e58b9590facfb89e3
Rukosuev Nikita <aletagro@yandex.ru>
2025-01-12 19:00:45 +0500

    test api

--- src/screens/Registration.js
+++ src/screens/Registration.js
@@ -54,8 +54,8 @@ const Registration = () => {
 
     const handleGetData = useCallback(async () => {
         // const result = await fetch('https://openlibrary.org/search/authors.json?q=j%20k%20rowling')
-        const result = await fetch('https://78.155.197.84/players/', {
-        // const result = await fetch('/players/', {
+        // const result = await fetch('https://78.155.197.84/players/', {
+        const result = await fetch('/players/', {
             headers: {
                 'Access-Control-Allow-Method': 'GET, OPTIONS',
                 'Access-Control-Allow-Origin': '*'
