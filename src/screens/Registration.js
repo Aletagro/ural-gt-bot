9d2e2ac007a0948b5f4ceffbaed7d3530fef46e4
Rukosuev Nikita <aletagro@yandex.ru>
2025-01-13 21:42:50 +0500

    first version reg

--- src/screens/Registration.js
+++ src/screens/Registration.js
@@ -13,9 +13,9 @@ const inputStyle = {
     '--Input-minHeight': '48px',
     'borderRadius': '4px',
     'margin': '16px',
-    'border-color': '#B4B4B4',
-    'box-shadow': 'none',
-    'font-family': 'Minion Pro Regular'
+    'borderColor': '#B4B4B4',
+    'boxShadow': 'none',
+    'fontFamily': 'Minion Pro Regular'
 }
 
 const cities = [
@@ -47,33 +47,38 @@ const Registration = () => {
     const [name, setName] = useState(user?.first_name || '')
     const [surname, setSurname] = useState(user?.last_name || '')
     const [city, setCity] = useState('')
+    const [userAlreadyReg, setUserAlreadyReg] = useState(false)
+    const [userId, setUserId] = useState(null)
 
     const isDisableButton = !name || !surname || !city
 
-    const [data, setData] = useState(null);
 
-    const handleGetData = useCallback(async () => {
-        // const result = await fetch('https://openlibrary.org/search/authors.json?q=j%20k%20rowling')
-        const result = await fetch('https://78.155.197.84/players/', {
-        // const result = await fetch('/players/', {
-            method: 'GET',
+    const handleRegUser = useCallback(async () => {
+        await fetch('https://78.155.197.84/players/reg', {
+            method: 'POST',
+            body: JSON.stringify({tgId: user?.id, name, surname, city}),
             headers: {
-                // 'Access-Control-Allow-Method': 'OPTIONS, TRACE, GET, HEAD, POST, PUT',
-                // 'Access-Control-Allow-Origin': '*',
-                // 'Access-Control-Allow-Headers': 'Content-Type, Authorization, Accept, X-Requested-With',
-                // 'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Accept': "application/json, text/javascript, /; q=0.01"
             }
         })
             .then(response => response.json())
-            .then(json => setData(json))
+            .then(json => {
+                setUserId(json?.player_info?.id)
+            })
             .catch(error => console.error(error))
-        console.log(result)
-      }, [])
-    console.log('data', data)
+      }, [name, surname, city, user?.id])
 
     useEffect(() => {
-        handleGetData()
-    }, [handleGetData])
+        fetch(`https://78.155.197.84/players/player/?q=${user?.id}`)
+            .then(response => response.json())
+            .then(json => {
+                if (json.tgId) {
+                    setUserAlreadyReg(true)
+                }
+            })
+            .catch(error => console.error(error))
+    }, [user?.id])
 
     const handleChangeName = (e) => {
         setName(e.target.value)
@@ -89,46 +94,56 @@ const Registration = () => {
 
     const handleClickButton = () => {
         setIsButtonPress(true)
+        handleRegUser()
     }
 
     return isButtonPress
-        ? <div>
-            <h2 id={Styles.title}>Вы успешно зарегистрированы на Ural GT 2025</h2>
-            <h3 id={Styles.title}>Ждём вас 1 марта в Екатеринбурге</h3>
-        </div>
-        : <div>
-            <h2 id={Styles.title}>Регистрация на Ural GT 2025</h2>
-            <h2 id={Styles.title}>{data && data[0]?.name}</h2>
-            <FloatingLabelInput
-                style={inputStyle}
-                onChange={handleChangeName}
-                label='Ваше имя'
-                value={name}
-            />
-            <FloatingLabelInput
-                style={inputStyle}
-                onChange={handleChangeSurname}
-                label='Ваша фамилия'
-                value={surname}
-            />
-            <Autocomplete
-                placeholder='Город'
-                onInputChange={handleChangeCity}
-                options={cities.sort()}
-                sx={inputStyle}
-                value={city}
-                autoComplete={true}
-                autoSelect={true}
-                freeSolo={true}
-            />
-            <div id={Styles.buttonContainer}>
-                <button
-                    id={isDisableButton ? Styles.disableButton : Styles.button}
-                    onClick={handleClickButton}
-                    disabled={isDisableButton}
-                >Зарегистрироваться</button>
+        ? userId > 60
+            ? <div id={Styles.container}>
+                <h2 id={Styles.title}>К сожалению, всем места на турнире уже заняты. Вы добавлены в лист ожидания Ural GT 2025</h2>
+                <h3 id={Styles.title}>Если места освободятся, то мы сразу с вами свяжемся</h3>
             </div>
-        </div>
+            : <div id={Styles.container}>
+                <h2 id={Styles.title}>Вы успешно зарегистрированы на Ural GT 2025</h2>
+                <h3 id={Styles.title}>Ждём вас 1 марта в Екатеринбурге</h3>
+            </div>
+        : userAlreadyReg
+            ? <div id={Styles.container}>
+                <h2 id={Styles.title}>Вы уже зарегистрированы на Ural GT 2025</h2>
+                <h3 id={Styles.title}>Ждём вас 1 марта в Екатеринбурге</h3>
+            </div>
+            : <div>
+                <h2 id={Styles.title}>Регистрация на Ural GT 2025</h2>
+                <FloatingLabelInput
+                    style={inputStyle}
+                    onChange={handleChangeName}
+                    label='Ваше имя'
+                    value={name}
+                />
+                <FloatingLabelInput
+                    style={inputStyle}
+                    onChange={handleChangeSurname}
+                    label='Ваша фамилия'
+                    value={surname}
+                />
+                <Autocomplete
+                    placeholder='Город'
+                    onInputChange={handleChangeCity}
+                    options={cities.sort()}
+                    sx={inputStyle}
+                    value={city}
+                    autoComplete={true}
+                    autoSelect={true}
+                    freeSolo={true}
+                />
+                <div id={Styles.buttonContainer}>
+                    <button
+                        id={isDisableButton ? Styles.disableButton : Styles.button}
+                        onClick={handleClickButton}
+                        disabled={isDisableButton}
+                    >Зарегистрироваться</button>
+                </div>
+            </div>
 }
 
 export default Registration
