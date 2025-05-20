372669e705ecb8883bea18cab2bb2a82e540c0ca
Rukosuev Nikita <aletagro@yandex.ru>
2025-05-20 18:10:08 +0500

    show all grand alliance

--- src/Constants.js
+++ src/Constants.js
@@ -2,27 +2,27 @@ import {accordionSummaryClasses} from '@mui/joy/AccordionSummary'
 
 const Constants = {
     grandAlliances: [
-        // {
-        //     name: 'Chaos',
-        //     id: '90175462-fae6-41e4-a0fe-19e41a833c9a',
-        //     image: 'https://dhss9aar8ocw.cloudfront.net/2a08adc1-4b87-43de-9c1a-7f8c9ca6699a'
-        // },
-        // {
-        //     name: 'Death',
-        //     id: '5c504c0c-cb25-4513-a137-7dd9efc172db',
-        //     image: 'https://dhss9aar8ocw.cloudfront.net/ed6e96e2-c8db-4bd1-bdc6-25e3ce953305'
-        // },
+        {
+            name: 'Chaos',
+            id: '90175462-fae6-41e4-a0fe-19e41a833c9a',
+            image: 'https://dhss9aar8ocw.cloudfront.net/2a08adc1-4b87-43de-9c1a-7f8c9ca6699a'
+        },
+        {
+            name: 'Death',
+            id: '5c504c0c-cb25-4513-a137-7dd9efc172db',
+            image: 'https://dhss9aar8ocw.cloudfront.net/ed6e96e2-c8db-4bd1-bdc6-25e3ce953305'
+        },
         {
             name: 'Destruction',
             shortName: 'Destr.',
             id: '3abb8417-72f9-47ab-a372-4d3f84c03caa',
             image: 'https://dhss9aar8ocw.cloudfront.net/bffedbb2-a415-484a-abac-97aa5e92b3b6'
         },
-        // {
-        //     name: 'Order',
-        //     id: 'ecb12990-a5de-4f3e-bc53-39d73855cbea',
-        //     image: 'https://dhss9aar8ocw.cloudfront.net/ae7d81ea-0642-4eae-9742-b0a061ff6feb'
-        // }
+        {
+            name: 'Order',
+            id: 'ecb12990-a5de-4f3e-bc53-39d73855cbea',
+            image: 'https://dhss9aar8ocw.cloudfront.net/ae7d81ea-0642-4eae-9742-b0a061ff6feb'
+        }
     ],
     armyEnhancements: [
         {
