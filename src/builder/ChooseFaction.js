b5184d51cae2a9169f45adbc3a19ec2724135660
Rukosuev Nikita <aletagro@yandex.ru>
2025-07-24 17:03:09 +0500

    add set saved roster

--- src/builder/ChooseFaction.js
+++ src/builder/ChooseFaction.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {useLocation} from 'react-router-dom'
-import {roster, navigationState} from '../utilities/appState'
-import {sortByName, cleanBuilder} from '../utilities/utils'
+import {roster, player, navigationState} from '../utilities/appState'
+import {sortByName, cleanBuilder, getStringAfterDash} from '../utilities/utils'
 import BuilderRow from './BuilderRow'
 import HeaderImage from '../components/HeaderImage'
 import Constants from '../Constants'
 
 import map from 'lodash/map'
+import find from 'lodash/find'
+import size from 'lodash/size'
+import compact from 'lodash/compact'
 import includes from 'lodash/includes'
 
 import Styles from './styles/ChooseFaction.module.css'
@@ -26,8 +29,50 @@ const ChooseFaction = () => {
     }
     sortByName(allegiances)
 
+    const getUnits = (units) => map(units, (unit) => {
+        const _unit = find(dataBase.data.warscroll, ['id', unit.id])
+        return {..._unit, ...unit}
+    })
+
+    const getTactic = (tacticName) => {
+        const tacticCard = find(dataBase.data.rule_container, (card) => getStringAfterDash(card.title) === tacticName)
+        return tacticCard ? {...tacticCard, name: tacticName} : null
+    }
+
+    const handleSetRoster = (list) => {
+        const regiments = map(list.regiments, (regiment) => {
+            const units = getUnits(regiment.units)
+            return {...regiment, units}
+        })
+        let tactics = []
+        if (size(list.tactics) > 0) {
+            tactics = compact(map(list.tactics, getTactic))
+        }
+        roster.id = list.id
+        roster.allegiance = list.allegiance
+        roster.allegianceId = list.allegianceId
+        roster.auxiliaryUnits = list.auxiliaryUnits ? getUnits(list.auxiliaryUnits) : []
+        roster.battleFormation = list.battleFormation
+        roster.factionTerrain = list.factionTerrain
+        roster.generalRegimentIndex = list.generalRegimentIndex
+        roster.grandAlliance = list.grandAlliance
+        roster.manifestationLore = list.manifestationLore
+        roster.manifestationsList = list.manifestationsList ? getUnits(list.manifestationsList) : []
+        roster.points = list.points || {all: 0}
+        roster.pointsLimit = list.pointsLimit
+        roster.prayersLore = list.prayersLore
+        roster.regimentOfRenown = list.regimentOfRenown ? find(dataBase.data.ability_group, ['id', list.regimentOfRenown?.id]) : null
+        roster.regiments = regiments
+        roster.regimentsOfRenownUnits = list.regimentsOfRenownUnits ? getUnits(list.regimentsOfRenownUnits) : []
+        roster.tactics = tactics
+        roster.spellsLore = list.spellsLore
+    }
+
     const handleClick = ({allegiance}) => {
-        if (roster.allegianceId !== allegiance.id) {
+        const list = JSON.parse(player?.roster || '')
+        if (list.allegianceId === allegiance.id) {
+            handleSetRoster(list)
+        } else if (roster.allegianceId !== allegiance.id) {
             cleanBuilder()
             roster.allegiance = allegiance.name
             roster.allegianceId = allegiance.id
